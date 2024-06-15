0af51a45f41b70bdef0c42d0ed440411f3396008
ZenseiTech <barizonte@gmail.com>
2024-06-14 23:42:33 -0400

    More work

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,13 @@
-"""Initialize the main blueprint."""
+"""Main blueprint."""
 from flask import Blueprint
 
+
+def do_import():
+    """Trick to bypass git_commit error ..."""
+    from . import views
+
+    views.logger
+
+
 main = Blueprint("main", __name__)
+do_import()
