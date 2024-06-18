98c628e8106435e1ff60cc493d7bc8a771623d9f
ZenseiTech <barizonte@gmail.com>
2024-06-17 21:40:20 -0400

    Cleaning

--- app/main/views.py
+++ app/main/views.py
@@ -9,20 +9,22 @@ from app.models import Author, Book
 from . import main
 
 
+def get_books():
+    """Return the books."""
+    return (
+        db.session.query(Book, Author).filter(Book.author_id == Author.author_id).all()
+    )
+
+
 @main.route("/", methods=["GET"])
 def home():
     """Start get endpoint."""
-    books = (
-        db.session.query(Book, Author).filter(Book.author_id == Author.author_id).all()
-    )
-    return render_template("index.html", books=books)
+    return render_template("index.html", books=get_books())
 
 
 @main.route("/submit", methods=["POST"])
 def submit():
     """Submission of a book."""
-    global_book_object = Book()
-
     title = request.form["title"]
     author_name = request.form["author"]
 
@@ -32,10 +34,9 @@ def submit():
     # check if author already exists in db
     if author_exists:
         author_id = author_exists.author_id
-        book = Book(author_id=author_id, title=title)
-        db.session.add(book)
+        book = Book.query.filter_by(author_id=author_id).first()
+        book.title = title
         db.session.commit()
-        global_book_object = book
     else:
         author = Author(name=author_name)
         db.session.add(author)
@@ -44,27 +45,7 @@ def submit():
         book = Book(author_id=author.author_id, title=title)
         db.session.add(book)
         db.session.commit()
-        global_book_object = book
-
-    response = f"""
-    <tr>
-        <td>{title}</td>
-        <td>{author_name}</td>
-        <td>
-            <button class="btn btn-primary"
-                hx-get="/get-edit-form/{global_book_object.book_id}">
-                Edit Title
-            </button>
-        </td>
-        <td>
-            <button hx-delete="/delete/{global_book_object.book_id}"
-                class="btn btn-primary">
-                Delete
-            </button>
-        </td>
-    </tr>
-    """
-    return response
+    return render_template("index.html", books=get_books())
 
 
 @main.route("/delete/<int:id>", methods=["DELETE"])
@@ -74,7 +55,7 @@ def delete_book(id):
     db.session.delete(book)
     db.session.commit()
 
-    return ""
+    return render_template("index.html", books=get_books())
 
 
 @main.route("/get-edit-form/<int:id>", methods=["GET"])
@@ -83,48 +64,7 @@ def get_edit_form(id):
     book = Book.query.get(id)
     author = Author.query.get(book.author_id)
 
-    response = f"""
-    <tr hx-trigger='cancel' class='editing' hx-get="/get-book-row/{id}">
-  <td><input name="title" value="{book.title}"/></td>
-  <td>{author.name}</td>
-  <td>
-    <button class="btn btn-primary" hx-get="/get-book-row/{id}">
-      Cancel
-    </button>
-    <button class="btn btn-primary" hx-put="/update/{id}" hx-include="closest tr">
-      Save
-    </button>
-  </td>
-    </tr>
-    """
-    return response
-
-
-@main.route("/get-book-row/<int:id>", methods=["GET"])
-def get_book_row(id):
-    """Get book by id."""
-    book = Book.query.get(id)
-    author = Author.query.get(book.author_id)
-
-    response = f"""
-    <tr>
-        <td>{book.title}</td>
-        <td>{author.name}</td>
-        <td>
-            <button class="btn btn-primary"
-                hx-get="/get-edit-form/{id}">
-                Edit Title
-            </button>
-        </td>
-        <td>
-            <button hx-delete="/delete/{id}"
-                class="btn btn-primary">
-                Delete
-            </button>
-        </td>
-    </tr>
-    """
-    return response
+    return render_template("edit.html", book=book, author=author, id=id)
 
 
 @main.route("/update/<int:id>", methods=["PUT"])
@@ -135,26 +75,4 @@ def update_book(id):
     )
     db.session.commit()
 
-    title = request.form["title"]
-    book = Book.query.get(id)
-    author = Author.query.get(book.author_id)
-
-    response = f"""
-    <tr>
-        <td>{title}</td>
-        <td>{author.name}</td>
-        <td>
-            <button class="btn btn-primary"
-                hx-get="/get-edit-form/{id}">
-                Edit Title
-            </button>
-        </td>
-        <td>
-            <button hx-delete="/delete/{id}"
-                class="btn btn-primary">
-                Delete
-            </button>
-        </td>
-    </tr>
-    """
-    return response
+    return render_template("index.html", books=get_books())
